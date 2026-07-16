f67f1a809b5181e3aaac562be7649c81a1540af4
Ashutosh <ashutoshau62@gmail.com>
2026-07-17 03:18:15 +0530

    Commiting the New Main.tf file in the repo

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "my_first_bucket" {
+  bucket = "your-unique-name-terraform-practice-2026"
+}
